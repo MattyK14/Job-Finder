8f0e25a5513ef80f36cbb75ffd8166bc633f9641
MattyK14 <xmattyk14x@hotmail.com>
2017-05-08 10:43:24 -0230

    Implement boilerplate redux

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 import Expo from 'expo';
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import { MainNavigator } from './screens/MainNavigator';
 
 class App extends React.Component {
   render() {
     return (
+      <Provider store={store}>
         <MainNavigator />
+      </Provider>
     );
   }
 }
